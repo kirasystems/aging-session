9efe739e6b2c011ced1c68f139622d513b41a0a6
Alexander K. Hudek <alexander@hudek.org>
2014-05-09 16:01:16 -0400

    Updated library versions.

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject aging-session "0.2.0"
+(defproject aging-session "0.2.1"
   :description "Memory based ring session with expiry and time based mutation."
   :url "https://github.com/diligenceengine/aging-session"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring/ring-core "1.1.0"]])
-
+  :dependencies [[ring/ring-core "1.2.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}})
