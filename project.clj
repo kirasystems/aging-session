4b2680d973f1624f868092cd8b57cb140774afc2
Gareth <gclapperton@live.com>
2016-05-06 22:23:43 -0400

    setup travis

--- project.clj
+++ project.clj
@@ -1,7 +1,21 @@
-(defproject aging-session "0.3.1"
+(defproject kirasystems/aging-session "0.3.1"
   :description "Memory based ring session with expiry and time based mutation."
   :url "https://github.com/diligenceengine/aging-session"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ring/ring-core "1.2.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}})
+
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :sign-releases false
+                              :username :env
+                              :password :env}]]
+
+  :dependencies [[ring/ring-core "1.4.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}}
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
