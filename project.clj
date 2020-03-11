d52a6df93bbab09f9b2bbd9bbf8f94ea3734e5d8
jpgauthier <j-p.g@hotmail.com>
2020-03-11 13:32:41 -0400

    Version 0.5.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kirasystems/aging-session "0.5.0"
+(defproject kirasystems/aging-session "0.5.1-SNAPSHOT"
   :description "Memory based ring session with expiry and time based mutation."
   :url "https://github.com/diligenceengine/aging-session"
   :license {:name "Eclipse Public License"
